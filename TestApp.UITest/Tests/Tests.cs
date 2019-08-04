3fcc951d09a5b98387c7174d751d2f0a472a0c43
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-08-04 12:05:05 +0200

    Revert "Revert "Add automation ids to all pages and renamed Text to Title""
    This reverts commit 3e28b68ff534b7e6cb91d6eccda28072c53614ba.

---
+++ TestApp.UITest/Tests/Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TestApp.UITest
+{
+    public class Tests : BaseTestFixture
+    {
+        public Tests(Platform platform) : base(platform) { }
+
+        [Test]
+        public void Repl()
+        {
+            if (TestEnvironment.IsTestCloud)
+            {
+                Assert.Ignore("Local only");
+            }
+            app.Repl();
+        }
+
+        [Test]
+        public void AddingItemUpdatesPage()
+        {
+            new ItemsPage()
+                .TapAdd();
+
+            new NewItemPage()
+                .EnterTitle("Item title")
+                .EnterDescription("Item description")
+                .TapSave();
+
+            new ItemsPage()
+                .VerifyNumberOfItems(1);
+        }
+
+        [Test]
+        public void CancellingReturnsToUnmodifiedItemsPage()
+        {
+            new ItemsPage()
+                .TapAdd();
+
+            new NewItemPage()
+                .TapCancel();
+
+            new ItemsPage()
+                .VerifyNumberOfItems(0);
+        }
+
+        [Test]
+        public void EditingItemUpdatesItemsPage()
+        {
+            new ItemsPage()
+                .TapAdd();
+
+            new NewItemPage()
+                .EnterTitle("Item title")
+                .EnterDescription("Item description")
+                .TapSave();
+
+            new ItemsPage()
+                .TapItem(0);
+
+            new ItemDetailPage()
+                .TapEdit();
+
+            new NewItemPage()
+                .EnterTitle("New item title")
+                .EnterDescription("New item description")
+                .TapSave();
+
+            new ItemsPage()
+                .CheckItem(0, "New item title", "New item description");
+        }
+    }
+}
---
+++ TestApp.UnitTest/ItemsViewModelTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApp.UnitTest
+{
+    [TestClass]
+    public class ItemsViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
