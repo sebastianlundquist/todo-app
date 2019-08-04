a7b502423d69964f7a0a6d2caaef405ec48631cc
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-08-04 17:50:43 +0200

    Add LoadItems_Correctly_Modifies_Items_Property test

--- TestApp.UnitTest/ItemsViewModelTests.cs
+++ TestApp.UnitTest/ItemsViewModelTests.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using TestApp.Models;
+using TestApp.Services;
+using TestApp.ViewModels;
 
 namespace TestApp.UnitTest
 {
@@ -6,8 +11,18 @@ namespace TestApp.UnitTest
     public class ItemsViewModelTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoadItems_Correctly_Modifies_Items_Property()
         {
+            var mockDb = new Mock<IDataStore<Item>>();
+            var vm = new ItemsViewModel(mockDb.Object);
+            var items = new List<Item>();
+            for (int i = 0; i < 5; i++)
+                items.Add(new Item());
+            mockDb.Setup(x => x.GetItems()).Returns(items);
+
+            vm.LoadItems();
+
+            Assert.AreEqual(5, vm.Items.Count);
         }
     }
 }
